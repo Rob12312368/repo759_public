3bd07789a7c27f62ee874a58854b58b51fee536f
Rob12312368 <rob12312368@gmail.com>
2024-09-15 12:20:27 +0800

    change cout to printf

--- HW01/task6.cpp
+++ HW01/task6.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-
+#include <stdio.h>
 int main(int argc, char *argv[]) {
     long long int n = std::stoll(argv[1]);
     for (long long int i=0; i<=n; i++)
     {
-        std::cout << i << " ";
+        printf("%lld ", i);
     }
     std :: cout << '\n';
     for (long long int i=n; i>=0; i--)
     {
         std::cout << i << " ";
     }
+
     return 0;
 }
