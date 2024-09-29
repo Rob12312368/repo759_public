ff560f69dd57a8a6b2037b7ac7ac3235067f2d6b
Rob12312368 <rob12312368@gmail.com>
2024-09-29 18:43:21 -0500

    add task2 sh

--- HW02/task2.cpp
+++ HW02/task2.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <random>
 #include "convolution.h"
+
+using std::chrono::high_resolution_clock;
+using std::chrono::duration;
+
 int main(int argc, char* argv[]){
     size_t n = std::stoul(argv[1]);
     size_t m = std::stoul(argv[2]);
     float* image = new float [n*n];
     float* mask = new float [m*m];
     float* output = new float [n*n];
+
+    high_resolution_clock::time_point start;
+    high_resolution_clock::time_point end;
+    duration<double, std::milli> duration_sec;
 
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -25,12 +33,15 @@ int main(int argc, char* argv[]){
         mask[i] = distrib_mask(gen);
     }
 
+    start = high_resolution_clock::now();
     convolve(image, output, n, mask, m);
+    end = high_resolution_clock::now();
 
-    for (size_t i = 0; i < n * n; i++)
-    {
-        std::cout << output[i] << " ";
-    }
+    duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
+    std::cout << duration_sec.count() << "\n";
+    std::cout << output[0] << "\n";
+    std::cout << output[n*n-1] << "\n";
+
     delete [] image;
     delete [] mask;
     delete [] output;
