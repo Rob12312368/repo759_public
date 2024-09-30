796c1d8c4860610f7ad0156db284d6a61ef5075d
Rob12312368 <rob12312368@gmail.com>
2024-09-30 13:11:35 -0500

    finish task3.cpp

--- HW02/task3.cpp
+++ HW02/task3.cpp
@@ -1,45 +1,85 @@
 #include <iostream>
 #include <random>
-#include "matmul.h"
 #include <vector>
+#include <chrono>
+#include "matmul.h"
+
 void zeroC(double* arr, size_t n){
     for (size_t i = 0; i < n; i++)
     {
         arr[i] = 0;
     }
 }
+
+using std::chrono::high_resolution_clock;
+using std::chrono::duration;
+
 int main(int argc, char* argv[]){
     size_t side = 1024;
     size_t arrsize = side * side;
     
     double* A = new double [arrsize];
     double* B = new double [arrsize];
-    std::vector<double>* A_vector = new std::vector<double>(arrsize, 1.0);
-    std::vector<double>* B_vector = new std::vector<double>(arrsize, 1.0);
     double* C = new double [arrsize];
-    for (size_t i  = 0; i < arrsize; i++)
+
+    high_resolution_clock::time_point start;
+    high_resolution_clock::time_point end;
+    duration<double, std::milli> duration_sec;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_real_distribution<> distrib_mask(-1.0, 1.0);
+
+    for (size_t i = 0; i < arrsize; i++)
     {
-        A[i] = 1.0;
-        B[i] = 1.0;
+        A[i] = distrib_mask(gen);
+        B[i] = distrib_mask(gen);
     }
 
+    const std::vector<double> A_vector(A, A + arrsize);
+    const std::vector<double> B_vector(B, B + arrsize);
+
+
+    std::cout << side << "\n";
+
+    start = high_resolution_clock::now();
     mmul1(A, B, C, side);
-    std::cout << C[arrsize - 1];
+    end = high_resolution_clock::now();
+    duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
+    std::cout << duration_sec.count() << "\n";
+    std::cout << C[arrsize - 1] << "\n";
+
     zeroC(C, arrsize);
+
+    start = high_resolution_clock::now();
     mmul2(A, B, C, side);
-    std::cout << C[arrsize - 1];
+    end = high_resolution_clock::now();
+    duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
+    std::cout << duration_sec.count() << "\n";
+    std::cout << C[arrsize - 1] << "\n";
+
     zeroC(C, arrsize);
+
+    start = high_resolution_clock::now();
     mmul3(A, B, C, side);
-    std::cout << C[arrsize - 1];
+    end = high_resolution_clock::now();
+    duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
+    std::cout << duration_sec.count() << "\n";
+    std::cout << C[arrsize - 1] << "\n";
+
     zeroC(C, arrsize);
-    mmul4(*A_vector, *B_vector, C, side);
-    std::cout << C[arrsize - 1];
+
+    start = high_resolution_clock::now();
+    mmul4(A_vector, B_vector, C, side);
+    end = high_resolution_clock::now();
+    duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
+    std::cout << duration_sec.count() << "\n";
+    std::cout << C[arrsize - 1] << "\n";
 
     delete [] A;
     delete [] B;
     delete [] C;
-    delete A_vector;
-    delete B_vector;
 
 
 
