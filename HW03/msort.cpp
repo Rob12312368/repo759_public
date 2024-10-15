e4891449c3d0aac05e78c9a8bd6481b8afd3b4d7
Rob12312368 <rob12312368@gmail.com>
2024-10-15 14:39:14 -0500

    fix merge sort

--- HW03/msort.cpp
+++ HW03/msort.cpp
@@ -1,8 +1,12 @@
 
 #include <algorithm>  // For std::sort
 #include "msort.h"
-
+#include <iostream>
 void mymerge(int* l_arr, int* r_arr, size_t l_size, size_t r_size){
+    for(size_t i = 0 ; i < l_size; i++)
+        std::cout << l_arr[i] << " ";
+    for(size_t i = 0 ; i < r_size; i++)
+        std::cout << r_arr[i] << " ";
     int tmparr[l_size+r_size];
     size_t l = 0, r = 0, i = 0;
     while (l < l_size and r < r_size){
@@ -16,19 +20,25 @@ void mymerge(int* l_arr, int* r_arr, size_t l_size, size_t r_size){
         }
         i++;
     }
+
     while(l < l_size)
     {
         tmparr[i] = l_arr[l];
+        i++;
         l++;
     }
     while(r < r_size)
     {
         tmparr[i] = r_arr[r];
+        i++;
         r++;
     }
     for(size_t j = 0; j < l_size + r_size; j++){
         l_arr[j] = tmparr[j];
+        //std::cout << tmparr[j];
     }
+    std::cout << "\n";
+    std::cout << "-----" << '\n';
     return;
 
 }
@@ -37,7 +47,6 @@ void mymerge(int* l_arr, int* r_arr, size_t l_size, size_t r_size){
 void msort(int* arr, const std::size_t n, const std::size_t threshold){
     if (n < threshold){
         std::sort(arr, arr+n);
-        std::cout << n;
         return;
     }
     if (n%2 == 0){
--- HW03/task3.cpp
+++ HW03/task3.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include "msort.h"
 int main(){
-    int a[6] = {5,4,3,2,-1,0};
+    int a[5] = {5,4,3,2,-1};
 
     msort(a, 5, 2);
-
+    std::cout << "here";
     for(int&t : a){
         std::cout << t;
     }
