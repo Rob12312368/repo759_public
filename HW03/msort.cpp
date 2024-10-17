3d1b1830eddceaaa60f66f40cdd2095dec36765c
Rob12312368 <rob12312368@gmail.com>
2024-10-16 20:27:45 -0500

    change array type from dynamic to static

--- HW03/msort.cpp
+++ HW03/msort.cpp
@@ -3,7 +3,7 @@
 #include "msort.h"
 #include <iostream>
 void mymerge(int* l_arr, int* r_arr, size_t l_size, size_t r_size){
-    int* tmparr = new int [l_size+r_size];
+    int tmparr[l_size+r_size];
     size_t l = 0, r = 0, i = 0;
     while (l < l_size and r < r_size){
         if (l_arr[l] <= r_arr[r]){
@@ -32,7 +32,6 @@ void mymerge(int* l_arr, int* r_arr, size_t l_size, size_t r_size){
     for(size_t j = 0; j < l_size + r_size; j++){
         l_arr[j] = tmparr[j];
     }
-    delete [] tmparr;
     return;
 
 }
